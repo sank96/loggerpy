92cb9e21303e6f146bef67ae0a0493997c37942e
Mattia <mattia.sanchioni.dev@gmail.com>
2020-12-28 00:00:52 +0100

    modified examples

--- loggerpy/tests/test_loggerpy.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import loggerpy
-
-class TestLogger(TestCase):
-    def test_is_logger(self):
-        logger = loggerpy.get_logger('test')
-        self.assertTrue(isinstance(logger, loggerpy._Logger))
